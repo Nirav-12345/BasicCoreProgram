49ea80498b0eb0b32fa8601fbe26d52654e9b9f7
Nirav-12345 <niravpandit0002@gmail.com>
2022-02-23 08:05:19 -0800

    adding class file

---
+++ QuioRemind.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuotientReminder
+{
+    public class QuioRemind
+    {
+        public static void FindingQuiotRemind()
+        {
+            int Divident = 50; int divisor = 8;
+
+            int Quiotent = Divident / divisor;
+
+            int Reminder = Divident % divisor;
+
+            Console.WriteLine("Divident" +Divident+ "divisor" + divisor);
+
+            Console.WriteLine("Quiotnet" + Quiotent);
+
+            Console.WriteLine("Reminder" + Reminder);
+        }
+    }
+}
