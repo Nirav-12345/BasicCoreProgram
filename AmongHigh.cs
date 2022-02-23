ff4f814eb402312004f4b9a6fa02253b8e507b33
Nirav-12345 <niravpandit0002@gmail.com>
2022-02-23 05:26:42 -0800

    adding program file

---
+++ AmongHigh.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LargestAmongThree
+{
+    public class AmongHigh
+    {
+
+        public static void LaNum()
+        {
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            int z = Convert.ToInt32(Console.ReadLine());
+
+            if (x > y)
+            {
+                
+
+
+
+
+
+             if (x> z)
+
+                {
+                    Console.WriteLine("The greatest int " + x);
+                }
+
+                else
+                {
+                    Console.WriteLine("The greatest int " + z);
+                }
+            }
+
+            else if(y>z)
+            {
+                Console.WriteLine("2nd is greatest");
+            }
+
+            else
+            {
+                Console.WriteLine("3rd is greatest among three");
+            }
+
+        }
+    }
+}
