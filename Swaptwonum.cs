9f4b89edb5a0f3a0b3086e4f0eb424c8f7d06f9e
Nirav-12345 <niravpandit0002@gmail.com>
2022-02-22 23:33:37 -0800

    adding program file

---
+++ Swaptwonum.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWaping_of_Two_Numbers
+{
+    public class Swaptwonum
+    {
+        public static void Swap()
+
+        {
+            int a = 5;
+            int b = 10;
+
+            a = a * b;
+            b = a / b;
+            a = a / b;
+
+            Console.WriteLine("Swap" + a );
+            Console.WriteLine("swap" + b);
+        }
+    }
+}
