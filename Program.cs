674fa54085c48c87439d2329eee1e3915c4fb387
Nirav-12345 <niravpandit0002@gmail.com>
2022-02-23 00:23:46 -0800

    adding class file

--- Program.cs
+++ Program.cs
@@ -1,31 +1,13 @@
 ﻿using System;
 
-namespace EvenOdd
+namespace BasicCoreProgram;
+
+class Program
+
 {
-    class Program
+    public static void Main(string[] args)
+
     {
-        public static void Main(string[] args)
-        {
-            double n = Convert.ToInt32(Console.ReadLine());
-
-            if (n % 2 == 0)
-
-            {
-
-                Console.WriteLine("Even");
-
-                
-
-            }
-
-            else
-            {
-                Console.WriteLine("odd");
-            }
-
-        }
-
-
+        VowelandConstnt.VowelToConsonant();
     }
 }
-
